f9862328efe649bf45843903df979bfa67d821e7
Inhyun Cho <xtra72@gmail.com>
2015-06-08 19:15:09 +0900

    Add SHT shell command

--- target/source/shell/fte_shell.c
+++ target/source/shell/fte_shell.c
@@ -69,6 +69,9 @@ const SHELL_COMMAND_STRUCT pSHELLCommands[] =
     { "lcd",        fte_ssd1305_shell_cmd},
 #endif
     { "save",       FTE_CFG_SHELL_cmdSave},
+#if FTE_SHT_SUPPORTED
+    { "sht",        FTE_SHT_SHELL_cmd},
+#endif
 #if FTE_SSL_SUPPORTED
     { "ssl",        FTE_SSL_SHELL_cmd},
 #endif
